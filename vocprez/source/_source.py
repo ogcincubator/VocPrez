697cabedcba3e91215b93f2a0486f64b1836509f
Alejandro Villar <avillar@ticnor.es>
2022-12-05 11:52:19 -0500

    Add missing prefix

--- vocprez/source/_source.py
+++ vocprez/source/_source.py
@@ -459,6 +459,7 @@ class Source:
     def get_vocabprops(self,vocab_uri):
         vocab = g.VOCABS[vocab_uri]
         q = """
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
             PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
